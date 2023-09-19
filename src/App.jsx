32437f6879229e3c3e6c8940d19e9b242aa9af17
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-09-19 22:07:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Name: header</div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+import packageJson from "./package.json";
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "header",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./Header": "./src/Header",
+        },
+        shared: {
+          ...packageJson.dependencies, // Access dependencies directly from package.json
+          react: {
+            singleton: true,
+            requiredVersion: packageJson.dependencies.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: packageJson.dependencies["react-dom"],
+          },
+        },
+      }),
+    ],
+    server: {
+      port: 8080,
+      proxy: {
+        // If you need to proxy requests to another server during development
+        // '/api': 'http://localhost:3000',
+      },
+      // Enable historyApiFallback for client-side routing
+      historyApiFallback: true,
+    },
+    build: {
+      target: "esnext",
+    },
+  };
+});
